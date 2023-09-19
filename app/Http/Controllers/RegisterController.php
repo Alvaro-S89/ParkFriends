6e36e0cc12b3612c234ef4b4b6847df272227c65
Alvaro <alvsr89@gmail.com>
2023-09-19 21:38:56 +0200

    feat: user registration

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use Faker\Guesser\Name;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class RegisterController extends Controller
 {
@@ -12,5 +14,25 @@ class RegisterController extends Controller
         return view('auth.register');
     }
 
+    public function store(Request $request)
+    {
+
+        $request->request->add(['username' => Str::slug($request->username) ]);
+        
+        $this->validate($request, [
+            'name' => 'required|max:20',
+            'username' => 'required|unique:users|min:3|max:18',
+            'email' => 'required|unique:users|email',
+            'password' => 'required|confirmed|min:8',
+            'password_confirmation' => 'required'
+        ]);
+
+        User::create([
+            'name' => $request->name,
+            'username' => $request->username,
+            'email' => $request->email,
+            'password' => Hash::make( $request->password )
+        ]);
+    }
 }
 
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -6,53 +6,71 @@
 
 @section('content')
         <div class="md:w-4/12 md:flex md:justify-center md:items-center bg-white p-6 rounded-lg shadow-2xl">
-            <form>
+            <form action="/create-account" method="POST">
                 @csrf
                 <div class="mb-5">
                     <label for="name" class="mb-2 block uppercase text-gray-500 font-bold">
                         Nombre
                     </label>
-                    <input class="border border-black p-2 rounded" 
+                    <input class="border border-black p-2 rounded @error('name') border-red-500 @enderror" 
                     type="text" 
                     id="name" 
                     name="name" 
                     placeholder="Tu nombre"
                     value="{{ old('name')}}"
                     >
+                    @error('name')
+                        <p class="text-red-500 my-2 text-sm p-2 text-center" >{{ $message }}</p>
+                    @enderror
                 </div>
+
                 <div class="mb-5">
                     <label for="username" class="mb-2 block uppercase text-gray-500 font-bold">
                         Nombre de usuario
                     </label>
-                    <input class="border border-black p-2 rounded" 
+                    <input class="border border-black p-2 rounded @error('username') border-red-500 @enderror" 
                     type="text" 
                     id="username" 
                     name="username" 
                     placeholder="Tu nombre de usuario"
+                    value="{{ old('username')}}"
                     >
+                    @error('username')
+                        <p class="text-red-500 my-2 text-sm p-2 text-center" >{{ $message }}</p>
+                    @enderror
                 </div>
+
                 <div class="mb-5">
                     <label for="email" class="mb-2 block uppercase text-gray-500 font-bold">
                         Email
                     </label>
-                    <input class="border border-black p-2 rounded" 
+                    <input class="border border-black p-2 rounded @error('email') border-red-500 @enderror" 
                     type="email" 
                     id="email" 
                     name="email" 
                     placeholder="Tu email"
+                    value="{{ old('email')}}"
                     >
+                    @error('email')
+                        <p class="text-red-500 my-2 text-sm p-2 text-center" >{{ $message }}</p>
+                    @enderror
                 </div>
+
                 <div class="mb-5">
                     <label for="password" class="mb-2 block uppercase text-gray-500 font-bold">
                         Contraseña
                     </label>
-                    <input class="border border-black p-2 rounded" 
+                    <input class="border border-black p-2 rounded @error('password') border-red-500 @enderror" 
                     type="password" 
                     id="password" 
                     name="password" 
                     placeholder="Tu contraseña"
                     >
+                    @error('password')
+                        <p class="text-red-500 my-2 text-sm p-2 text-center" >{{ $message }}</p>
+                    @enderror
                 </div>
+
                 <div class="mb-5">
                     <label for="password_confirmation" class="mb-2 block uppercase text-gray-500 font-bold">
                         Repetir contraseña
