3c9136b097de722ec167751fd18e3ffdca199dcd
Alvaro <alvsr89@gmail.com>
2023-09-30 22:13:11 +0200

    feat: show user, date and description of the post

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -5,7 +5,21 @@
 @endsection
 
 @section('content')
-    <div class="w-2/6 h-2/6">
-        <img src="{{ asset('uploads') . '/' . $post->image }}" alt="Post image">
+    <div class="container mx-auto flex">
+        <div class="md:w-1/2">
+            <img src="{{ asset('uploads') . '/' . $post->image }}" alt="Post image">
+            <div class="p-3">
+                <p>likes</p>
+            </div>
+            <div>
+                <p class="font-bold">{{$post->user->username}}</p>
+                <p class="text-sm text-gray-500">{{ $post->created_at->diffForHumans() }}</p>
+                <p class="mt-5">{{ $post->description }}</p>
+            </div>
+        </div>
+
+        <div class="md:w-1/2">
+            Coments
+        </div>
     </div>
 @endsection
