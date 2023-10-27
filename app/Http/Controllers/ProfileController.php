722dba881531ed4c62037d7f5967d17bc522db8f
Alvaro <alvsr89@gmail.com>
2023-10-27 13:31:12 +0200

    fix: change image without changing username

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -22,29 +22,26 @@ class ProfileController extends Controller
 
     public function store(Request $request)
     {
-
-        $request->request->add(['username' => Str::slug($request->username) ]);
-
         $this->validate($request, [
-            'username' => 'required|unique:users|min:2|max:18|not_in:profile',
+            'image' => 'image|mimes:jpeg,png,jpg,gif|max:2048',
+            'username' => 'unique:users,username,' . auth()->user()->id . '|min:2|max:18|not_in:profile',
         ]);
-
-        if($request->image){
-            $image = $request->file('image');
-
-            $imageName = Str::uuid() . "." . $image->extension();
-
-            $imageServer = Image::make($image);
-            $imageServer->fit(1000, 1000);
-            $imagepath = public_path('profiles') . '/' . $imageName;
-
-            $imageServer->save($imagepath);
-        }
 
         $user = User::find(auth()->user()->id);
 
-        $user->username = $request->username;
-        $user->image = $imageName ?? auth()->user()->image ?? null;
+        if ($request->image) {
+            $image = $request->file('image');
+            $imageName = Str::uuid() . "." . $image->extension();
+            $imageServer = Image::make($image);
+            $imageServer->fit(1000, 1000);
+            $imagepath = public_path('profiles') . '/' . $imageName;
+            $imageServer->save($imagepath);
+            $user->image = $imageName;
+        }
+
+        if ($request->has('username')) {
+            $user->username = $request->username;
+        }
 
         $user->save();
 
