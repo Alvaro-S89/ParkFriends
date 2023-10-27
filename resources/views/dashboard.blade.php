af89836fb889e1294ce4bfc1ebc6be811ab30685
Alvaro <alvsr89@gmail.com>
2023-10-27 18:02:01 +0200

    feat: round user image and add it in nav with view composer

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,7 +9,7 @@
     <div class="flex justify-center">
         <div class="w-full md:w-8/12 lg:w-6/12 flex flex-col items-center md:flex-row">
             <div class="w-8/12 lg:w-6/12 px-5">
-                <img src="{{ $user->image ? asset('profiles') . '/' . $user->image : asset('img/user.png')}}" alt="user">
+                <img src="{{ $user->image ? asset('profiles') . '/' . $user->image : asset('img/user.png')}}" alt="user" class="rounded-full">
             </div>
             <div class="md:w-8/12 lg:w-6/12 px-5 flex flex-col items-center py-10 md:justify-center md:items-start md:py-10">
                 <div class="flex items-center gap-2 mb-2">
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,8 +3,6 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        {{-- <link href="{{ asset('css/app.css') }}" rel="stylesheet"> --}}
-        
         
         <title>Laravel - @yield('title')</title>
         @stack('styles')
@@ -21,20 +19,20 @@
 
                 <nav class="flex gap-2 items-center">
                     @auth
-                    <a href="/posts/create" class="flex items-center gap-2 bg-white border p-2 mr-4 text-gray-600 rounded text-sm font-bold cursor-pointer">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 15.75l5.159-5.159a2.25 2.25 0 013.182 0l5.159 5.159m-1.5-1.5l1.409-1.409a2.25 2.25 0 013.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 001.5-1.5V6a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5 0 002.25 6v12a1.5 1.5 0 001.5 1.5zm10.5-11.25h.008v.008h-.008V8.25zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
-                        </svg>
-                        Crear
-                    </a>
-                    <a href="{{ route('posts.index', auth()->user()->username) }}">
-                        <img src="{{ asset('img/user.png')}}" class="w-10 h-10 mr-4" alt="user">
-                    </a>
-                    <form action="/logout" method="POST">
-                        @csrf
-                        <button type="submit" class="font-bold uppercase text-gray-600 text-sm">Logout</button>
-                    </form>
-                        @endauth
+                        <a href="/posts/create" class="flex items-center gap-2 bg-white border p-2 mr-4 text-gray-600 rounded text-sm font-bold cursor-pointer">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 15.75l5.159-5.159a2.25 2.25 0 013.182 0l5.159 5.159m-1.5-1.5l1.409-1.409a2.25 2.25 0 013.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 001.5-1.5V6a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5 0 002.25 6v12a1.5 1.5 0 001.5 1.5zm10.5-11.25h.008v.008h-.008V8.25zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
+                            </svg>
+                            Crear
+                        </a>
+                        <a href="{{ route('posts.index', auth()->user()->username) }}">
+                            <img src="{{ $user->image ? asset('profiles') . '/' . $user->image : asset('img/user.png')}}" class="rounded-full w-10 h-10 mr-4" alt="user">
+                        </a>
+                        <form action="/logout" method="POST">
+                            @csrf
+                            <button type="submit" class="font-bold uppercase text-gray-600 text-sm">Logout</button>
+                        </form>
+                    @endauth
                     @guest
                         <a href="/login" class="font-bold uppercase text-gray-600 text-sm">Login</a>
                         <a href="/create-account" class="font-bold uppercase text-gray-600 text-sm">Crear cuenta</a>
