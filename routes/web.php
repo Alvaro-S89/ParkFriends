318c833139fb969bd254147baee05fd5a0dc5666
Alvaro <alvsr89@gmail.com>
2023-11-06 12:50:13 +0100

    feat: new home page

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\CommentController;
 use App\Http\Controllers\FollowerController;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ImageController;
 use App\Http\Controllers\LikeController;
 use App\Http\Controllers\LoginController;
@@ -22,9 +23,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// //Closure route
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', HomeController::class);
+
 
 Route::get('/create-account', [RegisterController::class, 'index']);
 Route::post('/create-account', [RegisterController::class, 'store']);
