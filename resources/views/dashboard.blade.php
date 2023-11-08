1b9ec22c09e0492247f3b1393490b39e141bfc71
Alvaro <alvsr89@gmail.com>
2023-11-08 22:41:16 +0100

    feat: create component to show posts

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -75,25 +75,7 @@
     <section class="container mx-auto mt-10">
         <h2 class="text-4xl text-center font-black my-10">Publicaciones</h2>
 
-        @if ($posts->count())
-        
-            <div class="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5">
-                @foreach ($posts as $post)
-                <div>
-                    <a href="{{ route('posts.show', ['user' => $user, 'post' => $post ]) }}">
-                        <img src="{{ asset('uploads') . '/' . $post->image }}" alt="Post image">
-                    </a>
-                </div>
-                @endforeach
-            </div>
-
-            <div class="my-10">
-                {{ $posts->links() }}
-            </div>
-
-            @else
-                <p class="text-gray-600 text-center font-bold">Todavia no hay ninguna publicación😵</p>
-        @endif
+        <x-get-posts :posts="$posts" />
     </section>
     
 @endsection
